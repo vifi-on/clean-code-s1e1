5bb260c51baedb3e77945a5f91daa751f7deb4c0
vifi-on <ladiasman1992@gmail.com>
2021-03-26 15:07:22 +0300

    fix: fix js. point 1.3

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector("input[type=text]");
     var label=listItem.querySelector(".label");
     var editBtn=listItem.querySelector(".btn--edit");
     var containsClass=listItem.classList.contains("editMode");
